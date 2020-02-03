0d93d933ea958baabdf04386438ca15408982875
naokihtml <miyaki101033@gmail.com>
2020-02-03 18:29:41 +0900

    Initial Commit

---
+++ Swift5WKwebView1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5WKwebView1
+//
+//  Created by みや on 2020/02/03.
+//  Copyright © 2020 みや. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
